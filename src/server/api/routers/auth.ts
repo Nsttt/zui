a96dc4ff4e4aae01870486f1a4be296d127147f4
Nestor <elnestinatorHD@gmail.com>
2023-05-09 18:40:27 +0200

    Add register procedure

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -1,22 +1,43 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { prisma } from "@/server/db";
+import { TRPCError } from "@trpc/server";
+import crypto from "crypto";
+import { env } from "@/env.mjs";
 
-export const CreateUserSchema = z.object({
-  email: z.string().email(),
+export const UserSchema = z.object({
+  username: z.string(),
   password: z.string().min(4),
-  name: z.string(),
 });
 
 export const authRouter = createTRPCRouter({
-  createUser: protectedProcedure.input(CreateUserSchema).mutation(
-    async ({ input }) =>
+  createUser: protectedProcedure
+    .input(UserSchema)
+    .mutation(async ({ input }) => {
+      const exists = await prisma.user.findFirst({
+        where: { username: input.username },
+      });
+
+      if (exists) {
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: "User already exists",
+        });
+      }
+
+      const hash = crypto.pbkdf2Sync(
+        input.password,
+        env.NEXTAUTH_SECRET,
+        1000,
+        64,
+        "sha512"
+      );
+
       await prisma.user.create({
         data: {
-          email: input.email,
-          name: input.name,
-          password: input.password,
+          username: input.username,
+          password: hash.toString("hex"),
         },
-      })
-  ),
+      });
+    }),
 });
